16c2a3ea2683fc62e5deffc3f8445341a231d8e7
Ralf Geschke <ralf@kuerbis.org>
2020-09-30 17:42:44 +0200

    Domain commands complete

--- cmd/handleDomain.go
+++ cmd/handleDomain.go
@@ -7,25 +7,78 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/cobra"
 )
 
-func dbConn(dsn string) (db *sql.DB) {
-	/*if
-	dbname := os.Getenv("DBNAME")
-	dbhost := os.Getenv("DBHOST")
-	dbuser := os.Getenv("DBUSER")
-	dbpassword := os.Getenv("DBPASSWORD")
+var domainDsn string
 
-	db, err := sql.Open("mysql", dbuser+":"+dbpassword+"@tcp("+dbhost+":3306)/"+dbname)
-	if err != nil {
-		log.Println("Error by connecting database.")
-		panic(err.Error())
+func init() {
+	domainCmd.PersistentFlags().StringVarP(&domainDsn, "dsn", "d", "", "MySQL/MariaDB Data Source Name as described in https://github.com/go-sql-driver/mysql")
+
+	rootCmd.AddCommand(domainCmd)
+	domainCmd.AddCommand(domainListCmd)
+	domainCmd.AddCommand(domainAddCmd)
+
+}
+
+var domainCmd = &cobra.Command{
+	Use: "domain",
+
+	Short: "Manage domain entries",
+	Long:  `Manage dynpower domain entries in database.`,
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		handleDomain()
+	},
+}
+
+var domainListCmd = &cobra.Command{
+	Use: "list",
+
+	Short: "List domains in database",
+	Long:  `List all domains in the dynpower database. If a DSN is submitted by the flag --dsn, this DSN will be used. If no DSN is provided, dynpower-cli tries to use the environment variables DBHOST, DBUSER, DBNAME and DBPASSWORD.`,
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		list(domainDsn)
+	},
+}
+
+var domainAddCmd = &cobra.Command{
+	Use:   "add [domain] [access key]",
+	Short: "Add domain with access key to database",
+	Long:  `Add domain with access key to dynpower database. If a DSN is submitted by the flag --dsn, this DSN will be used. If no DSN is provided, dynpower-cli tries to use the environment variables DBHOST, DBUSER, DBNAME and DBPASSWORD.`,
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		add(domainDsn, args[0], args[1])
+	},
+}
+
+func dbConn(dsn string) (db *sql.DB) {
+	if len(dsn) < 1 {
+		dbname := os.Getenv("DBNAME") // maybe use LookupEnv to detect if env variable exists
+		dbhost := os.Getenv("DBHOST")
+		dbuser := os.Getenv("DBUSER")
+		dbpassword := os.Getenv("DBPASSWORD")
+		dbport := os.Getenv("DBPORT")
+		if len(dbport) < 1 {
+			dbport = "3306"
+		}
+		if len(dbname) >= 1 && len(dbhost) >= 1 && len(dbuser) >= 1 && len(dbpassword) >= 1 {
+			dsn = dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname
+
+		} else {
+			fmt.Println("No database connect parameter found, exiting. Please use --dsn or environment variables to define database connection.")
+			os.Exit(1)
+		}
+
 	}
-	return db*/
+
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Println("Error by connecting database.")
-		panic(err.Error())
+		fmt.Println("Error by connecting database.")
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 	return db
 }
@@ -44,7 +97,7 @@ func checkDb(dsn string) {
 	return
 }
 
-func List(dsn string) {
+func list(dsn string) {
 	db := dbConn(dsn)
 	//log.Println(dsn)
 	var maxStrlen int
@@ -119,31 +172,6 @@ func add(dsn string, domain string, accessKey string) {
 /*
 * Handle domain-related commands
  */
-func HandleDomain() {
-	//	func HandleDomain(fs *flag.FlagSet, dsn string) {
-	fmt.Println("handle Domain command")
-	//fmt.Println("DSN: " + dsn)
-
-	//fmt.Println(fs.Args())
-	/*switch fs.Arg(0) {
-	case "list":
-		//fmt.Println("Command: list")
-		list(dsn)
-	case "add":
-		//fmt.Println("Command: add")
-		domain := fs.Arg(1)
-		if len(domain) < 1 {
-			fmt.Println("\nDomain parameter missing. \n")
-			os.Exit(1)
-		}
-		accessKey := fs.Arg(2)
-		if len(accessKey) < 1 {
-			fmt.Println("\nAccess Key parameter missing. \n")
-			os.Exit(1)
-		}
-		add(dsn, domain, accessKey)
-	default:
-		fmt.Println("Unknown command.")
-	}*/
-
+func handleDomain() {
+	fmt.Println("\nUnknown or missing command.\nRun dynpower-cli domain --help to show available commands.")
 }
--- cmd/handleHost.go
+++ cmd/handleHost.go
@@ -2,12 +2,35 @@ package cmd
 
 import (
 	"fmt"
+
+	"github.com/spf13/cobra"
 )
+
+var hostDsn string
+
+func init() {
+	hostCmd.Flags().StringVarP(&hostDsn, "dsn", "d", "", "MySQL/MariaDB Data Source Name as described in https://github.com/go-sql-driver/mysql")
+
+	//domainCmd.AddCommand(domainListCmd)
+	rootCmd.AddCommand(hostCmd)
+
+}
+
+var hostCmd = &cobra.Command{
+	Use:   "host",
+	Short: "Handle host...Show details for an article",
+	Long:  `Details for an article`,
+	//Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		handleHost()
+	},
+}
 
 /*
 * Handle host-related commands
  */
-func HandleHost(dsn string) {
+func handleHost() {
 	fmt.Println("handle Host command")
 	/*fmt.Println(fs.Args())
 	switch fs.Arg(0) {
